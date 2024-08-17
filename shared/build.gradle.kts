3508a1c89efaac0f5b069a8c26f21c569a33a818
gusdoh <gus.doh@gmail.com>
2024-08-17 17:41:03 +1000

    0. Initialize: Make KMP project and basic dependencies.

---
+++ shared/build.gradle.kts
@@ -0,0 +1,61 @@
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.kotlinCocoapods)
+    alias(libs.plugins.androidLibrary)
+}
+
+kotlin {
+    androidTarget {
+        compilations.all {
+            kotlinOptions {
+                jvmTarget = "1.8"
+            }
+        }
+    }
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+
+    cocoapods {
+        summary = "Some description for the Shared Module"
+        homepage = "Link to the Shared Module homepage"
+        version = "1.0"
+        ios.deploymentTarget = "16.0"
+        podfile = project.file("../iosApp/Podfile")
+        framework {
+            baseName = "shared"
+            isStatic = true
+        }
+    }
+    
+    sourceSets {
+        commonMain.dependencies {
+            //put your multiplatform dependencies here
+            implementation(libs.kotlinx.coroutines.core)
+        }
+        commonTest.dependencies {
+            implementation(libs.kotlin.test)
+        }
+        androidMain.dependencies {
+            implementation(libs.androidx.lifecycle.viewmodel.ktx)
+           // implementation(libs.ktor.client.android)
+           // implementation(libs.sql.android.driver)
+        }
+        iosMain.dependencies {
+          // implementation(libs.ktor.client.darwin)
+          // implementation(libs.sql.native.driver)
+        }
+    }
+}
+
+android {
+    namespace = "pe.gusto.gapande"
+    compileSdk = 34
+    defaultConfig {
+        minSdk = 24
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
